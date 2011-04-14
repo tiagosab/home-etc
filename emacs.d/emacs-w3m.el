b9978787394828d83805c284d0dad8fe60082461
Tiago Saboga <tiagosaboga@gmail.com>
2011-04-14 11:01:36 -0300

    Move emacs files to emacs git archive.

--- emacs.d/emacs-w3m.el
+++
@@ -1,7 +0,0 @@
-; ~/.emacs-w3m
-
-(eval-after-load "w3m-search"
-  '(add-to-list 'w3m-search-engine-alist
-                '("pt.wikipedia"
-                  "http://pt.wikipedia.org/wiki/Especial:Search?search=%s"
-                  utf-8)))
