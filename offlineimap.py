ebe337d440721544d68fc40ecb5aa3217c70e505
Tiago Saboga <tiagosaboga@gmail.com>
2015-04-13 09:04:36 -0400

    offlineimap: new folders, new logic

--- offlineimap.py
+++ offlineimap.py
@@ -15,19 +15,34 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+'''offlineimap.py: my folder filter for offlineimap.'''
+
+
+folders = ['INBOX', 'pol/bspinoza', 'vf', 'auto-envio', 'herio',
+           'lixo', 'velhos/2009-2010', 'velhos/2009-2010/herio',
+           'velhos/2009-2010/virginia',
+           'velhos/2009-2010/incoming', 'Trash', 'Drafts',
+           '[Gmail]/Sent Mail', '[Gmail]/Trash', 'banco',
+           'hack/gitenberg-dev']
+
+recursivefolders = ['amigos']
 
 def main():
-    pass
+    with open('/home/tiago/.offlineimap.folders', 'w') as f:
+        f.write('')
 
 def ts_folderfilter(foldername):
     with open('/home/tiago/.offlineimap.folders', 'a') as f:
         f.write(foldername)
         f.write("\n")
-    return foldername in \
-      ['INBOX', 'pol/bspinoza', 'vf', 'auto-envio', 'herio', 'lixo', \
-      'velhos/2009-2010', 'velhos/2009-2010/herio', 'velhos/2009-2010/virginia', \
-       'velhos/2009-2010/incoming', 'Trash', 'Drafts', '[Gmail]/Sent Mail', \
-       '[Gmail]/Trash', 'banco']
+        include = False
+        if foldername in folders:
+            include = True
+        elif any([foldername.startswith(x) for x in recursivefolders]):
+            include = True
+        if include:
+            f.write("^^^^^^^^^^^^^^^^^^^^\n")
+    return include
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
